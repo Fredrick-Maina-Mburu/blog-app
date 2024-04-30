1f3752c82a31e4057c84ec394553c4c92e07d8da
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2024-04-30 13:58:51 +0300

    moved the header to the partial folder

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE php
+>
+<php
+ lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Responsice multipage blog website</title>
-  <link href="./style.css" rel="stylesheet">
+  <title>PHP & MySQL Blog Application with Admin Panel</title>
+  <link href="./css/style.css" rel="stylesheet">
 
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"> 
 
@@ -15,20 +17,28 @@
 <body>
   <nav>
     <div class="container nav_container">
-      <a href="index.html" class="nav_logo">FRED</a>
+      <a href="index.php
+      " class="nav_logo">FRED</a>
       <ul class="nav_items">
-        <li><a href="blog.html">Blog</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="services.html">Services</a></li>
-        <li><a href="contact.html">Contact</a></li>
-        <!-- <li><a href="sign-in.html">Sign in</a></li> -->
+        <li><a href="blog.php
+        ">Blog</a></li>
+        <li><a href="about.php
+        ">About</a></li>
+        <li><a href="services.php
+        ">Services</a></li>
+        <li><a href="contact.php
+        ">Contact</a></li>
+        <!-- <li><a href="sign-in.php
+        ">Sign in</a></li> -->
         <li class="nav_profile">
             <div class="avatar">
               <img src="./images/avatar1.jpg">
             </div>
             <ul>
-              <li><a href="dashboard.html">Dashboard</a></li>
-              <li><a href="logout.html">Log out</a></li>
+              <li><a href="dashboard.php
+              ">Dashboard</a></li>
+              <li><a href="logout.php
+              ">Log out</a></li>
             </ul>
         </li>        
       </ul>
@@ -45,7 +55,8 @@
       </div>
       <div class="post_info">
         <a href="" class="category_button">Wild Life</a>
-         <h2 class="post_title"><a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus.</a></h2>
+         <h2 class="post_title"><a href="post.php
+         ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus.</a></h2>
          <p class="post_body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, accusantium velit odit mollitia rem totam a! Amet accusantium ab saepe vel? Repudiandae corrupti libero laborum impedit sequi facere eaque? Expedita.</p>
          <div class="post_author">
           <div class="post_author-avatar">
@@ -71,7 +82,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -93,7 +105,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -115,7 +128,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -137,7 +151,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -159,7 +174,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -181,7 +197,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -203,7 +220,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -225,7 +243,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -247,7 +266,8 @@
         <div class="post_info">
           <a href="" class="category_button">Wild Life</a>
           <h3 class="post_title">
-            <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
+            <a href="post.php
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</a>
           </h3>
           <p class="post_body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab incidunt deleniti, nam sapiente dolor laboriosam corrupti ipsum asperiores dolorem corporis a iure aut nihil.</p>
           <div class="post_author">
@@ -343,4 +363,5 @@
 
   <script src="./main.js"></script>
 </body>
-</html>
+</php
+>
